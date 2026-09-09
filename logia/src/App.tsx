6b4b6d511982c9b49c9e666e76416feb20e19784
aamaury <amaury.aune@ynov.com>
2026-09-09 19:18:57 +0200

    [ADD] Check + display

--- logia/src/App.tsx
+++ logia/src/App.tsx
@@ -4,19 +4,30 @@ import { Grid } from "./Grid";
 import "./keyboard.css";
 import { Keyboard } from "./Keyboard";
 import { Modal } from "./Rules";
+import { Validate } from "./Validate";
+
+export interface Try {
+  guess: string;
+  result: string[];
+}
 
 function App() {
   const [currentGuess, setCurrentGuess] = useState("");
-  const [currentTry, setCurrentTry] = useState("");
+  const [result, setResult] = useState<string[]>([]);
+  const [history, setHistory] = useState<Try[]>([]);
 
   function handleKeyPress(key: string) {
     if (key === "<-") {
-      setCurrentGuess((prev) => prev.slice(0, -1)); 
+      setCurrentGuess((prev) => prev.slice(0, -1));
       return;
     }
     if (key === "ENTER") {
-      setCurrentTry(() => {})
-      return; 
+      const valid = Validate(currentGuess, "CHIEN");
+      setResult(valid);
+      setHistory([...history, { guess: currentGuess, result: valid }]);
+      setCurrentGuess("");
+      setResult([""]);
+      return;
     }
     if (currentGuess.length < 5) {
       setCurrentGuess((prev) => prev + key);
@@ -25,7 +36,7 @@ function App() {
 
   return (
     <>
-      <Grid currentGuess={currentGuess} />
+      <Grid currentGuess={currentGuess} result={result} history={history} />
       <Keyboard onKeyPress={handleKeyPress} />
     </>
   );
--- logia/src/Grid.tsx
+++ logia/src/Grid.tsx
@@ -1,56 +1,42 @@
-import grid from "./grid.module.css"
+import grid from "./grid.module.css";
+import type { Try } from "./App";
 
 interface GridProps {
   currentGuess: string;
+  result: string[];
+  history: Try[];
 }
 
-export function Grid({ currentGuess }: GridProps) {
-  const letters = currentGuess.split(""); 
+export function Grid({ currentGuess, result, history }: GridProps) {
+  const letters = currentGuess.split("");
 
   return (
     <div className={grid.grid}>
+      {history.map((essai, index) => (
+        <div className={grid.row} key={index}>
+          {[0, 1, 2, 3, 4].map((i) => (
+            <div className={`${grid.case} ${grid[essai.result[i]]}`} key={i}>
+              {essai.guess.split("")[i] ?? ""}
+            </div>
+          ))}
+        </div>
+      ))}
       <div className={grid.row}>
         {[0, 1, 2, 3, 4].map((i) => (
-          <div className={grid.case} key={i}>
+          <div className={`${grid.case} ${grid[result[i]]}`} key={i}>
             {letters[i] ?? ""}
           </div>
         ))}
       </div>
-      <div className={grid.row}>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-      </div>
-      <div className={grid.row}>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-      </div>
-      <div className={grid.row}>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-      </div>
-      <div className={grid.row}>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-      </div>
-      <div className={grid.row}>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-        <div className={grid.case}></div>
-      </div>
+      {Array(5 - history.length)
+        .fill("")
+        .map((_, i) => (
+          <div className={grid.row} key={i}>
+            {[0, 1, 2, 3, 4].map((j) => (
+              <div className={grid.case} key={j}></div>
+            ))}
+          </div>
+        ))}
     </div>
   );
 }
