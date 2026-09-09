182c518411b38995ef106fbd52e16f74b807b98a
aamaury <amaury.aune@ynov.com>
2026-09-09 11:25:00 +0200

    [ADD] Grid

--- logia/src/App.tsx
+++ logia/src/App.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import { Grid } from "./Grid";
+import "./grid.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid />
+    </>
+  );
 }
 
 export default App;
